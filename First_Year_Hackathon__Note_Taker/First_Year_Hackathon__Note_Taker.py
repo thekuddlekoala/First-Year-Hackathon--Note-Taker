6096be5203e15477935110d54ece20b05887ef7d
Raghav Jain <reach.raghav.jain@gmail.com>
2025-11-15 13:50:57 +0000

    Added email field

---
+++ First_Year_Hackathon__Note_Taker/First_Year_Hackathon__Note_Taker.py
@@ -0,0 +1,99 @@
+"""Welcome to Reflex! This file outlines the steps to create a basic app."""
+
+import reflex as rx
+import sqlite3
+
+conn = sqlite3.connect("NotesDB")
+cursor = conn.cursor()
+
+#Creating tables
+cursor.execute("""
+    PRAGMA foreign_keys = ON;
+
+-- ============================
+-- User Table
+-- ============================
+CREATE TABLE users (
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    
+    credits INTEGER,
+    isPremium BOOLEAN
+);
+
+-- ============================
+-- Image Table
+-- ============================
+CREATE TABLE images (
+    id INTEGER PRIMARY KEY,
+    content BLOB
+);
+
+-- ============================
+-- Note Table
+-- ============================
+CREATE TABLE notes (
+    id INTEGER PRIMARY KEY,
+    userid INTEGER,
+    upvote INTEGER,
+    downvote INTEGER,
+    imageid INTEGER,
+    tags TEXT,
+    date_of_creation TEXT NOT NULL --
+    -- ENUM visibility = {Public, Private, Shared}
+    visibility TEXT CHECK(visibility IN ('Public', 'Private', 'Shared')),
+    
+    -- ENUM academic_level = {GCSE, A-Level, University}
+    academic_level TEXT CHECK(academic_level IN ('GCSE', 'A-Level', 'University')),
+    
+    FOREIGN KEY (userid) REFERENCES user(id),
+    FOREIGN KEY (imageid) REFERENCES image(id)
+);
+
+-- ============================
+-- Note Shared With Table
+-- ============================
+CREATE TABLE note_shared_with (
+    id INTEGER PRIMARY KEY,
+    userid INTEGER,
+    noteid INTEGER,
+    
+    FOREIGN KEY (userid) REFERENCES user(id),
+    FOREIGN KEY (noteid) REFERENCES note(id)
+);
+
+               
+""")
+
+from rxconfig import config
+
+
+class State(rx.State):
+    """The app state."""
+
+
+def index() -> rx.Component:
+    # Welcome Page (Index)
+    return rx.container(
+        rx.color_mode.button(position="top-right"),
+        rx.vstack(
+            rx.heading("Welcome to Reflex!", size="9"),
+            rx.text(
+                "Get started by editing ",
+                rx.code(f"{config.app_name}/{config.app_name}.py"),
+                size="5",
+            ),
+            rx.link(
+                rx.button("Check out our docs!"),
+                href="https://reflex.dev/docs/getting-started/introduction/",
+                is_external=True,
+            ),
+            spacing="5",
+            justify="center",
+            min_height="85vh",
+        ),
+    )
+
+
+app = rx.App()
+app.add_page(index)
