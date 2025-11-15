1904cf3ccad5724590d7f2eb7b053f130ae83626
Andy Dishnica <104162882+andii-d@users.noreply.github.com>
2025-11-15 15:59:16 +0000

    Refactor User class into UserDAO for database handling

--- First_Year_Hackathon__Note_Taker/User.py
+++ First_Year_Hackathon__Note_Taker/User.py
@@ -1,13 +1,41 @@
 from datetime import datetime
 from Common import encryptSHA256
-import _sqlite3
+import sqlite3
 
 # Date time format
 DATE_FORMAT = "%d/%m/%Y"
 DB_FORMAT = "%d-%m-%Y"
 
-class User:
+class UserDAO:
+    def __init__(self, db_path: str = "NotesDB"):
+        self.db_path = db_path
+    
+    def _get_connection(self) -> sqlite3.Connection:
+        conn = sqlite3.connect(self.db_path)
+        conn.execute('PRAGMA foreign_keys = ON;')
+        return conn
+
+    def user_exists(self, user_id: int) -> bool:
+        try:
+            conn = sqlite3.connect(self.db_path)
+        
+            cursor = conn.cursor()
+            cursor.execute(f'SELECT id FROM users WHERE id = ?', (user_id))
+            res = cursor.fetchone() # Attempt to locate a row where the user's ID exists
+
+            return res is not None # Return true if the user's ID exists
+        
+        except sqlite3.Error as e:
+            print(f'Database Error: {e}')
+            return False
+
+        finally:
+            conn.close()
+
+class User(UserDAO):
     def __init__(self, id: int, name: str, email: str, password: str, credits: int, dob: str):
+        super().__init__() # Call the init of UserDAO
+
         self.id = id
         self.name = name
         self.password = password
@@ -41,54 +69,3 @@ class User:
             self.password = encryptSHA256(new_password)
         else:
             print('Temp error: Current user ID does not exist')
-    
-def has_special_chars(p):
-            for letter in p:
-                if not (letter.isalpha() or letter.isdigit() or letter == " "):
-                    return True
-            return False
-        
-        
-        #All the relevant validation for fields
-        
-        # def validate_email_syntax(email):
-        #     pattern = r"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$" #Function to validate whether the email is of the correct form.
-        #     return re.match(pattern, email) is not None
-            
-        # if validate_email_syntax(email) == False:
-        #     flash("Email not valid", category="error")
-        # #Checks if firstName/lastName has only letters and is more than 2 characters long
-        # elif len(firstName) < 2:
-        #     flash("Length of first name must be more than 2 characters", category="error")
-        # elif len(lastName) < 2:
-        #     flash("Length of last name must be more than 2 characters", category="error")
-        # elif any(char.isdigit() for char in firstName) == True or has_special_chars(firstName) == True:
-        #     flash("First Names must not have special characters/numbers", category="error")
-        # elif any(char.isdigit() for char in lastName) == True or has_special_chars(lastName) == True:
-        #     flash("Last Names must not have special characters/numbers", category="error")
-        # #Checks if password and confirm password match. 
-        # #Checks if it contains at least 1 special character, number, lower/uppercase character and is at least 8 characters.
-        # elif password1 != password2:   
-        #     flash("Passwords do not match", category="error")
-        # elif len(password1) < 8:
-        #     flash("Password must be greater than 8 characters in length", category="error")
-        # elif any(char.isdigit() for char in password1) == False:
-        #     flash("Password does not contain a number", category="error")
-        # elif any(char.isupper() for char in password1) == False:
-        #     flash("Password does not contain an uppercase character", category="error")
-        # elif any(char.islower() for char in password1) == False:
-        #     flash("Password does not contain an lowercase character", category="error")
-        # elif has_special_chars(password1) == False:
-        #     flash("Passwords do not contain special characters", category="error")
-        # else:
-        #     print("Hello")
-        #     if user_type == "tutor":
-        #         new_tutor = Tutor(f_name=firstName, s_name=lastName, email=email, user_password=generate_password_hash(password1, method="pbkdf2:sha256"))
-        #         db.session.add(new_tutor)
-        #         db.session.commit()
-        #         print("Added")
-        #     elif user_type == "student":
-        #         new_student = Student(f_name=firstName, s_name=lastName, email=email, user_password=generate_password_hash(password1, method="pbkdf2:sha256"))
-        #         db.session.add(new_student)
-        #         db.session.commit()
-        #         print("Added")
