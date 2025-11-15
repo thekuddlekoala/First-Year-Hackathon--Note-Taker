c8e2e06bf1d9db57d5f9a82b43f1d5ec0c0a6704
Raghav Jain <reach.raghav.jain@gmail.com>
2025-11-15 12:34:01 +0000

    Added the tables for SQLite using SQL code

--- First_Year_Hackathon__Note_Taker/First_Year_Hackathon__Note_Taker.py
+++ First_Year_Hackathon__Note_Taker/First_Year_Hackathon__Note_Taker.py
@@ -1,6 +1,68 @@
 """Welcome to Reflex! This file outlines the steps to create a basic app."""
 
 import reflex as rx
+import sqlite3
+
+conn = sqlite3.connect("NotesDB")
+cursor = conn.cursor()
+
+#Creating tables
+cursor.execute("""
+    PRAGMA foreign_keys = ON;
+
+-- ============================
+-- User Table
+-- ============================
+CREATE TABLE users (
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    credits INTEGER,
+    isPremium BOOLEAN
+);
+
+-- ============================
+-- Image Table
+-- ============================
+CREATE TABLE images (
+    id INTEGER PRIMARY KEY,
+    content BLOB
+);
+
+-- ============================
+-- Note Table
+-- ============================
+CREATE TABLE notes (
+    id INTEGER PRIMARY KEY,
+    userid INTEGER,
+    upvote INTEGER,
+    downvote INTEGER,
+    imageid INTEGER,
+    tags TEXT,
+    
+    -- ENUM visibility = {Public, Private, Shared}
+    visibility TEXT CHECK(visibility IN ('Public', 'Private', 'Shared')),
+    
+    -- ENUM academic_level = {GCSE, A-Level, University}
+    academic_level TEXT CHECK(academic_level IN ('GCSE', 'A-Level', 'University')),
+    
+    FOREIGN KEY (userid) REFERENCES user(id),
+    FOREIGN KEY (imageid) REFERENCES image(id)
+);
+
+-- ============================
+-- Note Shared With Table
+-- ============================
+CREATE TABLE note_shared_with (
+    id INTEGER PRIMARY KEY,
+    userid INTEGER,
+    noteid INTEGER,
+    
+    FOREIGN KEY (userid) REFERENCES user(id),
+    FOREIGN KEY (noteid) REFERENCES note(id)
+);
+
+               
+""")
 
 from rxconfig import config
 
